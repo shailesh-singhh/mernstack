a0fc21a1e08a5096ebead9311d920554771144b2
Shailesh singh <singhshailesh2901@gmail.com>
2025-03-23 12:49:39 +0530

    Java script learning

---
+++ Javascript/app.js
@@ -0,0 +1,11 @@
+console.log('Hello World!');
+let a = 10;
+let b = 20;
+console.log(a + b);
+let pP = 20;
+let eP = 100;
+console.log("The total profit is: " , eP + pP,"Rupees");
+let age = 20;
+if(age >= 18){
+    console.log("You are adult");
+}
---
+++ Javascript/tlight.js
@@ -0,0 +1,32 @@
+let color = "red";
+if(color == "red"){
+    console.log("Stop");
+}
+if(color == "yellow"){
+    console.log("Wait");
+}
+if(color == "blue"){
+    console.log("Go");
+}
+let size = 50;
+if(size === 250){
+    console.log("XL");
+}
+ else if(size === 200){
+    console.log("L");
+}
+ else if(size === 100){
+    console.log("M");
+}
+else if(size === 50){
+    console.log("S");
+}
+let str = "GoodString";
+if(str.length > 3 && str[0]==="a"){
+    console.log("Good String");
+}
+else{
+    console.log("Bad String");
+}
+alert("ERROR 404: Page Not Found");
+let rollno=prompt("Enter your rollno");
